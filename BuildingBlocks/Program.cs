e0f927a90a4580e8434321195229fe407de5a0b7
Havondor <Havondor@gmail.com>
2013-06-02 00:17:52 -0500

    More Work

--- BuildingBlocks/Program.cs
+++ BuildingBlocks/Program.cs
@@ -12,6 +12,10 @@ namespace BuildingBlocks
         {
             var myString = "Hello World";
             Console.WriteLine(myString);
+            int myFirstValue = 23;
+            double mySecondValue = 45.79;
+            double sum = myFirstValue + mySecondValue;
+            Console.WriteLine(sum);
         }
     }
 }
